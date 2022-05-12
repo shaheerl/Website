5368fe19d16bf01d2572dac88c35ce08a6a4563d
shaheerl <shaheerlone@gmail.com>
2022-05-12 17:11:05 -0400

    init

---
+++ main.js
@@ -0,0 +1,3 @@
+function addText() {
+    document.getElementById("addedDiv").textContent = 'See? Only someone with a high level of skill in programming can magically make text appear on the screen.';
+}
